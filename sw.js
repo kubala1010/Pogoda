3a77792af85be8973c586e8a41be7bc011c392fe
kubala1010 <kubala1010@gmail.com>
2026-08-22 23:17:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE_NAME = 'pogoda-cache-v1';
+const CACHE_NAME = 'pogoda-cache-v2';
 const urlsToCache = [
-  './pogoda.html',
+  './',
+  './index.html',
   './manifest.json',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
@@ -19,8 +20,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Zwróć plik z pamięci cache, jeśli istnieje (działa bez internetu)
-        // Jeśli nie, pobierz go normalnie z sieci
         return response || fetch(event.request);
       })
   );
